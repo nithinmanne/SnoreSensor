75d0c235460551a42fc5f9f3411268a13c8a5640
Naganithin Manne <nithinmanne@gmail.com>
2017-10-10 15:54:57 +0530

    Finished Code

--- app/src/main/java/com/naganithin/snoresensor/MainActivity.kt
+++ app/src/main/java/com/naganithin/snoresensor/MainActivity.kt
@@ -1,12 +1,121 @@
 package com.naganithin.snoresensor
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.media.AudioFormat
+import android.media.MediaRecorder
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.view.View
+import android.widget.Button
+import android.media.AudioRecord
+import android.widget.TextView
+import com.naganithin.fft.FFT
+
 
 class MainActivity : AppCompatActivity() {
+
+    private val requestCodeP = 0
+    private var recording = false
+    private val SAMPLE_RATE = 44100
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val permissions = arrayOf(Manifest.permission.RECORD_AUDIO)
+        ActivityCompat.requestPermissions(this, permissions, requestCodeP)
     }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == requestCodeP && grantResults[0] != PackageManager.PERMISSION_GRANTED)
+            ActivityCompat.requestPermissions(this, permissions, requestCodeP)
+    }
+
+    fun startRec() {
+        Thread(Runnable {
+            android.os.Process.setThreadPriority(android.os.Process.THREAD_PRIORITY_AUDIO)
+
+            var bufferSize = AudioRecord.getMinBufferSize(SAMPLE_RATE,
+                    AudioFormat.CHANNEL_IN_MONO,
+                    AudioFormat.ENCODING_PCM_16BIT)
+            if (bufferSize == AudioRecord.ERROR || bufferSize == AudioRecord.ERROR_BAD_VALUE) {
+                bufferSize = SAMPLE_RATE * 2
+            }
+
+            val record = AudioRecord(MediaRecorder.AudioSource.DEFAULT,
+                    SAMPLE_RATE,
+                    AudioFormat.CHANNEL_IN_MONO,
+                    AudioFormat.ENCODING_PCM_16BIT,
+                    bufferSize)
+
+            val audioBuffer = ShortArray(bufferSize / 2)
+
+            if (record.state != AudioRecord.STATE_INITIALIZED) {
+                return@Runnable
+            }
+            record.startRecording()
+
+            var shortsRead: Long = 0
+            val fft = FFT(1024)
+            var snore = 0
+            val text = findViewById<TextView>(R.id.textView)
+
+            while (recording) {
+                val numberOfShort = record.read(audioBuffer, 0, audioBuffer.size)
+                shortsRead += numberOfShort.toLong()
+
+                val y = DoubleArray(1024)
+                val x = audioBuffer.map { it.toDouble() }.toDoubleArray()
+
+                fft.fft(x, y)
+                val j = PES(x)
+
+                if (j==1) {
+                    snore++
+                    if (snore>5) {
+                        runOnUiThread {
+                            text.text = getString(R.string.yes)
+                            snore = 0
+                            Thread.sleep(2000)
+                        }
+                    }
+                }
+                else {
+                    snore = 0
+                    runOnUiThread { text.text = getString(R.string.no) }
+                }
+            }
+            record.stop()
+            record.release()
+            runOnUiThread { text.text = "" }
+        }).start()
+    }
+
+    fun PES(a: DoubleArray): Int {
+        val E_L = (0..51).sumByDouble { a[it] * a[it] }
+        val E_H = (53..511).sumByDouble { a[it] * a[it] }
+        val pes = E_L / (E_L + E_H)
+        if (pes < 0.65)
+            return 1
+        else
+            return 0
+    }
+
+    fun record(v: View) {
+        recording = !recording
+        val button = findViewById<Button>(R.id.button)
+        val text = findViewById<TextView>(R.id.textView)
+        if (recording) {
+            button.text = getString(R.string.stop)
+            text.text = getString(R.string.no)
+            startRec()
+        }
+        else {
+            button.text = getString(R.string.start)
+        }
+        print(v)
+    }
+
 }
